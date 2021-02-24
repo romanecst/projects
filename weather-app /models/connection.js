a7b82daf8cf47e7ac536f14b0ec66210bc7e4a25
romanecst <72913790+romanecst@users.noreply.github.com>
2021-02-24 14:49:32 +0000

    Update connection.js

--- weather-app /models/connection.js
+++ weather-app /models/connection.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
+const URI = require('./URI');
 
 var options = {
   connectTimeoutMS: 5000,
   useNewUrlParser: true,
   useUnifiedTopology : true
 }
-mongoose.connect('mongodb+srv://romane:roma04@cluster0.bu6a5.mongodb.net/weatherapp?retryWrites=true&w=majority', 
+mongoose.connect(URI, 
     options,         
     function(err) {
      console.log(err);
